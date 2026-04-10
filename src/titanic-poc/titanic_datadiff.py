0b85aadbfb20298ecdad4e5845e8bf46e4c7550d
Xavi Planas <xavi.planas.peres@gmail.com>
2026-04-10 20:15:45 +0200

    Add debug and dict structures for each DB

--- src/titanic-poc/titanic_datadiff.py
+++ src/titanic-poc/titanic_datadiff.py
@@ -2,10 +2,14 @@ from titanic_utils import mysql_engine, mysql_engine_url, postgres_engine_url, d
 from data_diff import connect_to_table, diff_tables, disable_tracking
 from sqlalchemy import inspect
 
+
 disable_tracking()
+
+DEBUG = True
 
 table_raw = dataset["raw"]["table"]
 table_modified = dataset["modified"]["table"]
+primary_key = ("PassengerId")
 
 mysql_metadata = inspect(mysql_engine)
 mysql_all_columns=[col["name"] for col in mysql_metadata.get_columns(table_raw)]
@@ -14,7 +18,7 @@ mysql_all_columns=[col["name"] for col in mysql_metadata.get_columns(table_raw)]
 table_mysql = connect_to_table(
     f"mysql://{mysql_engine_url}",
     table_name=table_raw,
-    key_columns=("PassengerId"),
+    key_columns=primary_key,
     extra_columns = mysql_all_columns
 )
 
@@ -28,15 +32,23 @@ table_pg = connect_to_table(
 added = 0
 removed = 0
 total = 0
+left = []
+right = []
+
 #for diff in diff_tables(table_mysql, table_pg, extra_columns=mysql_all_columns):
 for diff in diff_tables(table_mysql, table_pg):
    total+=1
+   d = dict(zip(mysql_all_columns, diff[1][1:]))
    if diff[0] == '+' :
       added+=1
+      right.append(d)
+      DEBUG and print(f"postgreSQL {d}")
    elif diff[0] == '-' :
       removed+=1
+      left.append(d)
+      DEBUG and print(f"mySQL {d}")
 
-   print(diff)
+   DEBUG and print(diff)
 
 print("Cambios detectados en total:", total)
 print("Insertados:", added)
